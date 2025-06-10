9dd45764525932163e5e7203089e697244a0c3ca
duongpokeeeeee <29dangcap@gmail.com>
2025-06-11 01:34:47 +0700

    Update b2_batdau.sh

--- b2_batdau.sh
+++ b2_batdau.sh
@@ -7,9 +7,9 @@ MIN=$1
 MAX=$2
 while true; do
     if [ -z "$MAX" ]; then
-        python3 main.py "$MIN" --dataset=data.txt
+        python3 python_learning.py "$MIN" --dataset=data.txt
     else
-        python3 main.py "$MIN","$MAX" --dataset=data.txt
+        python3 python_learning.py "$MIN","$MAX" --dataset=data.txt
     fi
     sleep 15
 done
