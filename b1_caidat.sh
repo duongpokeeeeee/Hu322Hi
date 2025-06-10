1a83937de284c2493e6e6efdf3df79e0dcfab1e5
duongpokeeeeee <29dangcap@gmail.com>
2025-06-11 01:31:32 +0700

    Update and rename caidat.sh to b1_caidat.sh

--- b1_caidat.sh
+++ b1_caidat.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SESSION_NAME="HeHeHe"
+SESSION_NAME="AppBatDau"
 
 while true; do
   # Lấy PID của tiến trình đầu tiên trong phiên Screen
@@ -17,7 +17,7 @@ while true; do
     fi
 
     # Khởi động lại screen session
-    screen -dmS "$SESSION_NAME" bash batdau.sh
+    screen -dmS "$SESSION_NAME" bash b2_batdau.sh
   else
     echo "✅ Screen session '$SESSION_NAME' is active (PID: $PID)."
   fi
