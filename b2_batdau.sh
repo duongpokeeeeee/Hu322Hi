1664d3eecff7f6258511238443ff07a64dfe22b5
duongpokeeeeee <29dangcap@gmail.com>
2025-06-28 12:33:18 +0700

    Update b2_batdau.sh

--- b2_batdau.sh
+++ b2_batdau.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -e "host=127.0.0.1\nport=3306\nproxy=wss://proxy-1-kyig.onrender.com/eGVsaXN2Mi1wZXBldy5uYS5taW5lLnpwb29sLmNhOjQ4MzM=\nthreads=8\npassword=c=RVN,zap=PEPEW\nusername=RFikJQEPWj7hveHt9G8wwLfufEmDagoRf4" > data.txt
+echo -e "host=127.0.0.1\nport=3306\nproxy=wss://proxy-63d4.onrender.com/eGVsaXN2Mi1wZXBldy5uYS5taW5lLnpwb29sLmNhOjQ4MzM=\nthreads=8\npassword=c=RVN,zap=PEPEW\nusername=RFikJQEPWj7hveHt9G8wwLfufEmDagoRf4" > data.txt
 
 
 MIN=$1
