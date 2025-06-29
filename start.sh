b46b02f1001d99a258f64dc9997c17a6ebf3812e
duongpokeeeeee <29dangcap@gmail.com>
2025-06-29 22:38:26 +0700

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-MIN=$1
-URL="https://api.npoint.io/c42052220f3a30904017"
-
-curl -s "$URL" | jq -r 'to_entries | map("\(.key)=\(.value)") | .[]' > data.txt
-while true; do
-    python3 main.py $MIN --dataset=data.txt --output=result.txt
-    sleep 30
-done
